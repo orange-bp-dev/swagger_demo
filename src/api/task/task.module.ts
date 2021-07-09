4d99f5c5abd6d4d8b30d1f9ea3b9997b9a8d2287
Sato Ryoka <satoryoka0920@gmail.com>
2021-07-09 19:21:16 +0900

    Cache Manager

--- src/api/task/task.module.ts
+++ src/api/task/task.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskController } from './task.controller';
 import { TaskRepository } from './task.repository';
 import { TaskService } from './task.service';
+import { Cache } from 'cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), CacheModule.register()],
   controllers: [TaskController],
   providers: [TaskService],
 })
--- src/api/task/task.service.ts
+++ src/api/task/task.service.ts
@@ -1,24 +1,36 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  Inject,
+  CACHE_MANAGER,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { TaskRepository } from './task.repository';
+import { Cache } from 'cache-manager';
+import axios from 'axios';
 
 @Injectable()
 export class TaskService {
   constructor(
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getTaskById(id: number): Promise<Task> {
-    const found = await this.taskRepository.findOne({ where: { id } });
+    const cached_task: Task = await this.cacheManager.get('task');
 
-    if (!found) {
-      throw new NotFoundException(`Task with ID ${id} no found`);
+    if (cached_task) {
+      return cached_task;
+    } else {
+      const found = await this.taskRepository.findOne({ where: { id } });
+      if (!found) {
+        throw new NotFoundException(`Task with ID ${id} no found`);
+      }
+      await this.cacheManager.set('task', found, { ttl: 3600 });
+      return found;
     }
-
-    return found;
+    // return found;
   }
-
-  private tasks: Task[] = [];
 }
